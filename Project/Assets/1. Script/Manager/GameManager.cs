005dff095f0afc165a354cbf0d2515d9038fae9f
kbmhansungb <kbmhansungb@gmail.com>
2024-08-10 21:52:05 +0900

    Unity 6.0 업데이트

--- Project/Assets/1. Script/Manager/GameManager.cs
+++ Project/Assets/1. Script/Manager/GameManager.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using UnityEngine;
 
 public enum EGameState
@@ -18,6 +23,26 @@ public partial class GameManager : MonoBehaviour
     [SerializeField] private MainPresenter mainPresenter;
 
     private EGameState gameState = EGameState.Main;
+
+    Socket sock;
+    public static string[] GetAllLocalIPv4(NetworkInterfaceType _type)
+    {
+        List<string> ipAddrList = new List<string>();
+        foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+            {
+                foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddrList.Add(ip.Address.ToString());
+                    }
+                }
+            }
+        }
+        return ipAddrList.ToArray();
+    }
 
     private void Start()
     {
